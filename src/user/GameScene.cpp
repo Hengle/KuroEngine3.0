05c115461bc0fb2f1db957900b364325749e0604
hassy560 <k020g1167@g.neec.ac.jp>
2022-04-27 12:29:21 +0900

    ゲームシーンプッシュ

--- src/user/GameScene.cpp
+++ src/user/GameScene.cpp
@@ -4,14 +4,46 @@
 
 GameScene::GameScene()
 {
+	ligMgr.RegisterDirLight(&dirLig);
+	ligMgr.RegisterPointLight(&ptLig);
 }
 
 void GameScene::OnInitialize()
 {
+	debugCam.Init({ 0,1,-3 }, { 0,1,0 });
 }
 
 void GameScene::OnUpdate()
 {
+	static const float UINT = 0.1f;
+	auto ptLigPos = ptLig.GetPos();
+	if (UsersInput::Instance()->KeyInput(DIK_W))
+	{
+		ptLigPos.y += UINT;
+	}
+	if (UsersInput::Instance()->KeyInput(DIK_S))
+	{
+		ptLigPos.y -= UINT;
+	}
+	if (UsersInput::Instance()->KeyInput(DIK_D))
+	{
+		ptLigPos.x += UINT;
+	}
+	if (UsersInput::Instance()->KeyInput(DIK_A))
+	{
+		ptLigPos.x -= UINT;
+	}
+	if (UsersInput::Instance()->KeyInput(DIK_E))
+	{
+		ptLigPos.z += UINT;
+	}
+	if (UsersInput::Instance()->KeyInput(DIK_Q))
+	{
+		ptLigPos.z -= UINT;
+	}
+	ptLig.SetPos(ptLigPos);
+
+	debugCam.Move();
 }
 
 #include"DrawFunc3D.h"
@@ -19,7 +51,10 @@ void GameScene::OnUpdate()
 void GameScene::OnDraw()
 {
 	static auto testModel = Importer::Instance()->LoadFBXModel("resource/user/player/", "player.fbx");
-	static Camera cam("test");
+	static auto testglTF0 = Importer::Instance()->LoadGLTFModel("resource/user/gltf/", "player.glb");
+	//static auto testglTF1 = Importer::Instance()->LoadGLTFModel("resource/user/gltf/", "player.gltf");
+	//static auto testglTF2 = Importer::Instance()->LoadGLTFModel("resource/user/gltf/", "player_anim_test.gltf");
+
 	static Transform trans;
 	static std::shared_ptr<DepthStencil>dsv = D3D12App::Instance()->GenerateDepthStencil(
 		D3D12App::Instance()->GetBackBuffRenderTarget()->GetGraphSize());
@@ -27,8 +62,22 @@ void GameScene::OnDraw()
 	dsv->Clear(D3D12App::Instance()->GetCmdList());
 
 	KuroEngine::Instance().Graphics().SetRenderTargets({ D3D12App::Instance()->GetBackBuffRenderTarget() }, dsv);
-	DrawFunc3D::DrawNonShadingModel(testModel, trans, cam);
 
+	static bool DRAW_FLG = false;
+	if (DRAW_FLG)
+	{
+		DrawFunc3D::DrawShadingModel(ligMgr, testglTF0, trans, debugCam);
+	}
+	else
+	{
+		DrawFunc3D::DrawShadingModel(ligMgr, testModel, trans, debugCam);
+	}
+
+	if (UsersInput::Instance()->KeyOnTrigger(DIK_R))DRAW_FLG = !DRAW_FLG;
+
+
+	//DrawFunc3D::DrawNonShadingModel(testModel, trans, debugCam);
+	//DrawFunc3D::DrawNonShadingModel(testglTF0, trans, debugCam);
 }
 
 void GameScene::OnImguiDebug()
--- src/user/GameScene.h
+++ src/user/GameScene.h
@@ -1,8 +1,13 @@
 #pragma once
 #include"KuroEngine.h"
+#include"DebugCamera.h"
+#include"LightManager.h"
 class GameScene : public BaseScene
 {
-
+	DebugCamera debugCam;
+	LightManager ligMgr;
+	Light::Direction dirLig;
+	Light::Point ptLig;
 public:
 	GameScene();
 	void OnInitialize()override;
