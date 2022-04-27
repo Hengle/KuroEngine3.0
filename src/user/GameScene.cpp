3557231dc2afd14f7a730edcf6a453ac1ed6b0bf
hassy560 <k020g1167@g.neec.ac.jp>
2022-04-27 13:39:18 +0900

    MT4 運動の法則後編課題

--- src/user/GameScene.cpp
+++ src/user/GameScene.cpp
@@ -4,6 +4,19 @@
 static const Vec2<float>INIT_VEL = { 10,10 };
 static const float RADIUS = 32.0f;
 
+void GameScene::EmitParticles(const int& Num, const Vec2<float>& EmitPos, const Vec2<float>& EmitVec, const Angle& RandAngle)
+{
+	int num = 0;
+	for (auto& p : particles)
+	{
+		if (p.life)continue;
+		float rotateAngle = KuroFunc::GetRand(-RandAngle / 2.0f, RandAngle / 2.0f);
+		p.Emit(EmitPos, KuroMath::RotateVec2(EmitVec, rotateAngle));
+		num++;
+		if (Num <= num)break;
+	}
+}
+
 GameScene::GameScene()
 {
 
@@ -13,120 +26,155 @@ void GameScene::OnInitialize()
 {
 	pos = WinApp::Instance()->GetExpandWinCenter();
 
-	if (!accelMove)
+	vel = { 0,0 };
+	accel = { 0,0 };
+
+	for (auto& p : particles)
 	{
-		vel = INIT_VEL;
-	}
-	else
-	{
-		vel = { 0,0 };
-		accel = { 0,0 };
+		p.Init();
 	}
 }
 
 #include"KuroMath.h"
 void GameScene::OnUpdate()
 {
+	static const float GRAVITY = 0.1f;
+	static const float MAX_ACCEL_Y = 5.0f;
+	static const float AIR_FRICTION = 0.99f;
+
+	//パーティクル更新
+	for (auto& p : particles)
+	{
+		if (!p.life)continue;
+
+		//空気抵抗X
+		p.accel.x *= AIR_FRICTION * 0.9f;
+
+		//重力
+		p.accel.y += GRAVITY;
+
+		//最大落下加速度
+		if (MAX_ACCEL_Y < p.accel.y)p.accel.y = MAX_ACCEL_Y;
+
+		p.vel += p.accel;
+		//速度の減衰
+		p.vel = KuroMath::Lerp(p.vel, { 0,0 }, 0.3f);
+
+		p.pos += p.vel;
+
+		p.life--;
+	}
+
+	//プレイヤー入力と更新
 	bool up = UsersInput::Instance()->KeyInput(DIK_W);
 	bool down = UsersInput::Instance()->KeyInput(DIK_S);
 	bool left = UsersInput::Instance()->KeyInput(DIK_A);
 	bool right = UsersInput::Instance()->KeyInput(DIK_D);
 	float skewRate = cos(Angle::ConvertToRadian(45));
 
-	//等速移動
-	if (!accelMove)
+	static const float ACCEL = 0.5f;
+	//地面との摩擦
+	const float groundFrictionRate = onGround ? 1.0f : 0.08f;
+	if (left)
 	{
-		if (left)
+		accel.x -= ACCEL * groundFrictionRate * ((up || down) ? skewRate : 1.0f);
+
+		if (onGround)
 		{
-			pos.x -= vel.x * ((up || down) ? skewRate : 1.0f);
-		}
-		if (right)
-		{
-			pos.x += vel.x * ((up || down) ? skewRate : 1.0f);
-		}
-		if (up)
-		{
-			pos.y -= vel.y * ((left || right) ? skewRate : 1.0f);
-		}
-		if (down)
-		{
-			pos.y += vel.y * ((left || right) ? skewRate : 1.0f);
+			EmitParticles(1, { pos.x,pos.y + RADIUS }, Vec2<float>(1.0f, -1.0f), Angle(45));
 		}
 	}
-	//加速で移動
-	else
+	if (right)
 	{
-		static const float ACCEL = 0.2f;
-		if (left)
+		accel.x += ACCEL * groundFrictionRate * ((up || down) ? skewRate : 1.0f);
+
+		if (onGround)
 		{
-			accel.x -= ACCEL * ((up || down) ? skewRate : 1.0f);
+			EmitParticles(1, { pos.x,pos.y + RADIUS }, Vec2<float>(-1.0f, -1.0f), Angle(45));
 		}
-		if (right)
-		{
-			accel.x += ACCEL * ((up || down) ? skewRate : 1.0f);
-		}
-
-
-		//重力が有効
-		if (addGravity)
-		{
-			static const float GRAVITY = 0.1f;
-			accel.y += GRAVITY;
-
-			//ジャンプ
-			static const float JUMP_POWER = -3.0f;
-			if (UsersInput::Instance()->KeyOnTrigger(DIK_SPACE))
-			{
-				accel.y = JUMP_POWER;
-			}
-		}
-		//WSキー有効（トップビュー移動）
-		else
-		{
-			if (up)
-			{
-				accel.y -= ACCEL * ((left || right) ? skewRate : 1.0f);
-			}
-			if (down)
-			{
-				accel.y += ACCEL * ((left || right) ? skewRate : 1.0f);
-			}
-		}
-
-		//速度加算
-		vel += accel;
-		pos += vel;
-
-		//速度の減衰
-		vel = KuroMath::Lerp(vel, { 0,0 }, 0.3f);
 	}
+
+	//重力
+	accel.y += GRAVITY;
+
+	//最大重力加速度制限（空気抵抗Y）
+	if (MAX_ACCEL_Y < accel.y)
+	{
+		accel.y = MAX_ACCEL_Y;
+	}
+
+	//ジャンプ（空中ジャンプ可）
+	static const float JUMP_POWER = -2.5f;
+	if (UsersInput::Instance()->KeyOnTrigger(DIK_SPACE))
+	{
+		accel.y = JUMP_POWER;
+	}
+
+	//速度加算
+	vel += accel;
+	pos += vel;
+
+	//速度の減衰
+	vel = KuroMath::Lerp(vel, { 0,0 }, 0.3f);
 
 	//画面外に出ないよう制限
 	const auto winSize = WinApp::Instance()->GetExpandWinSize();
 	static const auto REFLECT_RATE = -0.8f;
+
+	//壁との摩擦
+	static const float WALL_FRICTION_RATE = 0.8f;
+	bool wallHit = false;
+
+	//右
 	if (winSize.x < pos.x + RADIUS)
 	{
 		pos.x = winSize.x - RADIUS;
 		accel.x = 0.0f;
+		accel.y *= WALL_FRICTION_RATE;	//摩擦
 		vel.x *= REFLECT_RATE;
+		wallHit = true;
+		EmitParticles(1, { pos.x + RADIUS,pos.y }, Vec2<float>(-1.0f, -1.0f), Angle(45));
 	}
+	//左
 	if (pos.x - RADIUS < 0.0f)
 	{
 		pos.x = RADIUS;
 		accel.x = 0.0f;
+		accel.y *= WALL_FRICTION_RATE;	//摩擦
 		vel.x *= REFLECT_RATE;
+		wallHit = true;
+		EmitParticles(1, { pos.x - RADIUS,pos.y }, Vec2<float>(1.0f, -1.0f), Angle(45));
 	}
+	//下
 	if (winSize.y < pos.y + RADIUS)
 	{
 		pos.y = winSize.y - RADIUS;
 		accel.y = 0.0f;
+		accel.x *= WALL_FRICTION_RATE;	//摩擦
 		vel.y *= REFLECT_RATE;
+		if (!onGround)
+		{
+			EmitParticles(1, { pos.x,pos.y + RADIUS }, Vec2<float>(0.0f, -1.0f), Angle(45));
+		}
+		onGround = true;
+		wallHit = true;
 	}
+	else onGround = false;
+	//上
 	if (pos.y - RADIUS < 0.0f)
 	{
 		pos.y = RADIUS;
 		accel.y = 0.0f;
+		accel.x *= WALL_FRICTION_RATE;	//摩擦
 		vel.y *= REFLECT_RATE;
+		wallHit = true;
+		EmitParticles(1, { pos.x,pos.y - RADIUS }, Vec2<float>(0.0f, 1.0f), Angle(45));
+	}
+
+	//空気抵抗X
+	if (!wallHit)
+	{
+		accel.x *= AIR_FRICTION;
 	}
 }
 
@@ -134,6 +182,19 @@ void GameScene::OnUpdate()
 void GameScene::OnDraw()
 {
 	KuroEngine::Instance().Graphics().SetRenderTargets({ D3D12App::Instance()->GetBackBuffRenderTarget() });
+
+	for (auto& p : particles)
+	{
+		if (!p.life)continue;
+
+		static const float MAX_SIZE = 10.0f;
+		const float lifeRate = (p.lifeSpan - p.life) / (float)p.lifeSpan;
+		const float size = KuroMath::Ease(In, Circ, lifeRate, MAX_SIZE, 0.0f);
+		const Vec2<float>leftUpPos = p.pos - Vec2<float>(size, size);
+		const Vec2<float>rightBottomPos = p.pos + Vec2<float>(size, size);
+		DrawFunc2D::DrawBox2D(leftUpPos, rightBottomPos, Color(1.0f, 1.0f, 1.0f, 1.0f - lifeRate), true, AlphaBlendMode_Trans);
+	}
+
 	DrawFunc2D::DrawCircle2D(pos, RADIUS, Color(), true);
 }
 
@@ -141,36 +202,8 @@ void GameScene::OnImguiDebug()
 {
 	ImGui::Begin("Parameters");
 
-	if (ImGui::Checkbox("AccelMove", &accelMove))
-	{
-		if (!accelMove)vel = INIT_VEL;
-		else
-		{
-			vel = { 0,0 };
-			accel = { 0,0 };
-		}
-	}
-
-	ImGui::Indent(32.0f);
-
-	if (ImGui::Checkbox("AddGravoty", &addGravity))
-	{
-		accel = { 0,0 };
-		vel = { 0,0 };
-		if (!accelMove)accelMove = true;
-	}
-
-	ImGui::Separator();
-
-	if (addGravity)
-	{
-		ImGui::Text("Move - AD");
-		ImGui::Text("Space - Jump");
-	}
-	else
-	{
-		ImGui::Text("Move - WASD");
-	}
+	ImGui::Text("Move - AD");
+	ImGui::Text("Space - Jump");
 
 	ImGui::End();
 }
--- src/user/GameScene.h
+++ src/user/GameScene.h
@@ -1,18 +1,43 @@
 #pragma once
 #include"KuroEngine.h"
 #include"LightManager.h"
-
 #include<memory>
+#include<array>
 
 class GameScene : public BaseScene
 {
+	struct Particle
+	{
+		Vec2<float>pos;
+		Vec2<float>vel;
+		Vec2<float>accel;
+		int life;
+		int lifeSpan;
+		void Init() { life = 0; }
+		void Emit(const Vec2<float>& EmitPos, const Vec2<float>& EmitVec)
+		{
+			pos = EmitPos;
+			vel = { 0,0 };
+			static const float POWER_MAX = 3.0f;
+			static const float POWER_MIN = 1.0f;
+			accel = EmitVec * KuroFunc::GetRand(POWER_MIN, POWER_MAX);
+			static const int LIFE_MAX = 30;
+			static const int LIFE_MIN = 8;
+			lifeSpan = KuroFunc::GetRand(LIFE_MIN, LIFE_MAX);
+			life = lifeSpan;
+		}
+	};
+	static const int PARTICLE_MAX = 100;
+	std::array<Particle, PARTICLE_MAX>particles;
+
 	Vec2<float>pos;
 	Vec2<float>vel;
 
-	bool accelMove = false;
 	Vec2<float>accel;
 
-	bool addGravity = false;
+	bool onGround = false;
+
+	void EmitParticles(const int& Num, const Vec2<float>& EmitPos, const Vec2<float>& EmitVec, const Angle& RandAngle);
 
 public:
 	GameScene();
